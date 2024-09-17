53edda2974064ca5dd851fa4bce896211c8e5f2f
valritehachi <valritehachi@gmail.com>
2024-09-17 15:21:56 +0000

    edits

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This function determines the smallest number of coins needed
+   to get to a given total
+"""
+
+def makeChange(coins, total):
+    """This function makes changes to coins and 
+    calculates that change.
+    """
+    if total <= 0:
+        return 0
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
